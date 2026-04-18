d52b3fb07082b579135a74ffb81047e7f826cb5a
PieWol <75956460+PieWol@users.noreply.github.com>
2026-04-18 10:37:01 +0000

    fix copyright

--- example/basic/18_ckks_sorting.cpp
+++ example/basic/18_ckks_sorting.cpp
@@ -1,7 +1,3 @@
-// Copyright 2024-2026 Alişah Özcan
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
 /**
  * @file 18_ckks_sorting.cpp
  *
